74498a6f393e8d05c037ad03e6a51f2b23de9073
vitoria221 <129074652+vitoria221@users.noreply.github.com>
2023-09-15 07:35:34 -0300

    Add files via upload

---
+++ bhaskara.py
@@ -0,0 +1,33 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+a = float(input('Entre com o valor de a: '))
+b = float(input('Entre com o valor de b: '))
+c = float(input('Entre com o valor de c: '))
+
+delta = (b ** 2) - 4 * a * c
+
+print("\n**************************\n")
+
+if a == 0:
+    print("O valor de a, deve ser diferente de 0")
+elif delta < 0:
+    print("Sem raízes reais")
+else:
+    x1 = (-b + delta ** (1 / 2)) / (2 * a)
+    x2 = (-b - delta ** (1 / 2)) / (2 * a)
+
+    print("x1: {}, x2: {}".format(x1, x2))
